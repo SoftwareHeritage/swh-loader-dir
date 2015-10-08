a1a99622e47f3500da1899e353fcdebb954b4163
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2015-10-08 10:30:12 +0200

    Add comment + license header

--- scratch/walking.py
+++ scratch/walking.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+# Tryouts scratch buffer
+# Not for production
+
+# Copyright (C) 2015  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
 import os
 import shutil
 import tempfile
